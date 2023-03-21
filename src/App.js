a230d5347973c4dab09f94700c8047383228863b
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-03-21 12:51:41 +0530

    Project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/Home'; 
+import Header from './components/Header'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header">
+      </Header>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
